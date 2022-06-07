1056dfb50133a637133f3918fc5b2d0d76206bb3
Robin Quique <robin.quique@ccomptes.fr>
2022-06-07 09:54:26 +0200

    Fix celery.pid

--- start.sh
+++ start.sh
@@ -4,6 +4,6 @@ python manage.py collectstatic --noinput
 celery multi start worker1\
         --beat -A ecc -l info\
         --scheduler=django_celery_beat.schedulers:DatabaseScheduler\
-        --pidfile=\
+        --pidfile=/var/log/celery.pid\
         --logfile=/var/log/ecc-celery.log
 gunicorn -c gunicorn_config.py ecc.wsgi:application
