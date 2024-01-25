294ed7fa37f4e3cc593644beacd3031fc39f513f
aaarrouf <achraf.aarrouf-prestataire@ccomptes.fr>
2024-01-25 11:27:15 +0100

    Cursor closed : set DISABLE_SERVER_SIDE_CURSORS to True

--- ecc/settings.py
+++ ecc/settings.py
@@ -389,6 +389,10 @@ ENV_NAME = env("ENV_NAME", default="")
 # https://mattsegal.dev/file-logging-django.html
 #  APP_NAME come from gunicorn config
 FILENAME = env("APP_NAME", default=None)
+
+# Activer DISABLE_SERVER_SIDE_CURSORS
+DISABLE_SERVER_SIDE_CURSORS = True
+
 if not DEBUG and FILENAME:
     LOGGING = {
         "version": 1,
