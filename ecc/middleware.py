2eceeb3a956b0090de085a447e0de50eaea2f0e5
aaarrouf <achraf.aarrouf-prestataire@ccomptes.fr>
2024-01-29 12:10:22 +0100

    Check email domain

--- ecc/middleware.py
+++ ecc/middleware.py
@@ -13,7 +13,14 @@ class ExternalAccessMiddleware:
     # Vérifier si EXTERNAL_ACCESS est défini et vaut 1
     if external_access_header  and external_access_header == '1':
 
-      return HttpResponseForbidden("L'accès à l'application est restreint aux utilisateurs externes.")
+      user_email = request.user.username if request.user.is_authenticated else None
+
+      denied_domains  = ['ccomptes.fr', 'hcfp.fr']
+
+      # Vérifier le domaine de l'e-mail
+      if user_email and any(user_email.endswith(domain) or user_email.endswith(f'.{domain}') for domain in denied_domains ):
+
+        return HttpResponseForbidden("Votre accès est bloqué car l'utilisation de cet e-mail n'est pas autorisée. Veuillez utiliser le VPN de la Cour sur votre poste de travail pour accéder à e-Controle.")
 
     response = self.get_response(request)
 
